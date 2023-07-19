e75c739b88461c53b421b2aa469c588100d0a277
Yuhao <455076227@qq.com>
2023-07-19 15:30:50 +0800

    first commit

---
+++ src/main/java/com/atqingniao/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atqingniao.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.atqingniao.git
+ * Description:
+ *
+ * @Author: 唐玉亮
+ * @Craete:2023/7/19 - 15:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
